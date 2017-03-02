134ca7ed9887dee7b2ce456a98319aae1de78eec
Evan Gray <evanlawgray@gmail.com>
2017-03-01 20:08:44 -0800

    Bind report encounter form to capture data from user (not inc. date & colonist ID).

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import { NewEncounter, Alien, Colonist } from '../models';
 
 @Component({
   selector: 'app-report',
@@ -6,8 +8,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report.component.scss']
 })
 export class ReportComponent implements OnInit {
+  newEncounter: NewEncounter;
+  colonist: Colonist;
+  aliens: Alien[];
+  date: Date;
+  reportForm: FormGroup;
+  submitted: boolean;
 
-  constructor() { }
+
+  constructor() {
+    this.aliens = [
+      {
+        "type": "Special K",
+        "submitted_by": "1",
+        "id": 1,
+        "description": "Special."
+      },
+      {
+        "type": "Endomorph",
+        "submitted_by": "1",
+        "id": 2,
+        "description": "Slimy, and gross."
+      },
+      {
+        "type": "Octospider",
+        "submitted_by": "1",
+        "id": 3,
+        "description": "Rendevous with Rama."
+      },
+      {
+        "type": "The Predator",
+        "submitted_by": "1",
+        "id": 4,
+        "description": "Yeah, he's here. Call Arnold."
+      },
+      {
+        "type": "Darth Vader",
+        "submitted_by": "3",
+        "id": 5,
+        "description": "Got daddy issues."
+      },
+      {
+        "type": "Yoda",
+        "submitted_by": "3",
+        "id": 6,
+        "description": "Do or do not do; there is not try."
+      }
+    ];
+
+    this.submitted = false;
+
+    this.reportForm = new FormGroup ({
+      atype: new FormControl('', [Validators.required]),
+      action: new FormControl('', [Validators.required, Validators.maxLength(200)])
+  })
+
+   }
+
+   logEncounter(){
+     this.submitted = true;
+     console.log(this.reportForm);
+   }
 
   ngOnInit() {
   }
