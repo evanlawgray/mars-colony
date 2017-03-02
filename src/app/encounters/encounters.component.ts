f1931d93217b4fcfec4fd0384a0e7db14f380dea
Evan Gray <evanlawgray@gmail.com>
2017-03-01 20:29:50 -0800

    Set up encounters form to populate page with encounters array (no ajax yet).

--- src/app/encounters/encounters.component.ts
+++ src/app/encounters/encounters.component.ts
@@ -1,13 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import {Encounter} from '../models';
 
 @Component({
   selector: 'app-encounters',
   templateUrl: './encounters.component.html',
-  styleUrls: ['./encounters.component.scss']
+  styleUrls: ['./encounters.component.scss'],
 })
 export class EncountersComponent implements OnInit {
+  encounters: Encounter[]
 
-  constructor() { }
+  constructor() { 
+    this.encounters = [
+        {
+        "id": 3,
+        "date": "2016-11-18",
+        "colonist_id": 8,
+        "atype": "The Predator",
+        "action": "Rubbed mud on self to hide from heat sensors."
+      },
+      {
+        "id": 4,
+        "date": "2016-11-18",
+        "colonist_id": 10,
+        "atype": "Special K",
+        "action": "Hunted the alien known as \"Special K\".\nHe offered donuts for his life. Took donuts, killed anyway."
+      },
+      {
+        "id": 5,
+        "date": "2016-11-18",
+        "colonist_id": 18,
+        "atype": "Octospider",
+        "action": "Removed the legs, made some soup."
+      },
+      {
+        "id": 6,
+        "date": "2016-11-18",
+        "colonist_id": 13,
+        "atype": "Special K",
+        "action": "Saw Ermil eating all of the Doughnuts.  Told him to eat a carrot! He did.\n"
+      },
+      {
+        "id": 7,
+        "date": "2016-11-18",
+        "colonist_id": 16,
+        "atype": "Octospider",
+        "action": "asf"
+      },
+    ];
+  }
 
   ngOnInit() {
   }
