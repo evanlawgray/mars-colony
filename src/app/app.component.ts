4b58cdac0b3617a5c6b300c86231a913c0f42f43
Evan Gray <evanlawgray@gmail.com>
2017-03-05 15:21:21 -0800

    Restyle submit encounter page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    
+    <a routerLink="/register">register page</a>
+    <a routerLink="/encounters">encounters page</a>
+    <a routerLink="/report">report encounters page</a>
+    <a routerLink="/notfound">404 not found page</a>
     <div class="page">
       <router-outlet></router-outlet>
     </div>
